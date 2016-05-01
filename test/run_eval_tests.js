e4c14c2a45a273e3ffa3d001647ab458d330fe9e
Matej Krajčovič <elbarto512@gmail.com>
2016-05-01 21:13:26 +0200

    Add tests for ES6 WeakMaps

--- test/run_eval_tests.js
+++ test/run_eval_tests.js
@@ -16,4 +16,4 @@ function test(source) {
 ['basic', 'array', 'getters_setters', 'left_side_of_assignment',
  'detect_directive', 'flexible_directives', 'safeGetAttr', 'this',
  'immutable_map_success', 'immutable_map_fail', 'immutable_map_default',
- 'map_success', 'map_fail'].map(test);
+ 'map_success', 'map_fail', 'weak_map_success', 'weak_map_fail'].map(test);
